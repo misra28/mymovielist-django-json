6afc0024ba3d3d661da08087379e4ed9e1802547
Ahvan Misra <misra28@purdue.edu>
2024-05-08 16:30:39 -0700

    added field in admin

--- movielist/admin.py
+++ movielist/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from .views import get_movie_json
 
 # Register your models here.
 @admin.register(models.User)
@@ -9,5 +10,15 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(models.ListEntry)
 class ListEntryAdmin(admin.ModelAdmin):
-    list_display = ['user', 'movie_id', 'rating', 'date_watched', 'comments']
+    list_display = ['user', 'movie_id', 'title', 'rating', 'date_watched', 'comments']
     autocomplete_fields = ['user']
+    
+    def title(self, obj):
+        return get_movie_json(obj.movie_id)['title']
+    
+    """def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _title=get_movie_json(self.movie_id),
+        )
+        return queryset"""
