0552b7d81ef4ce0b0b43849e24dde4f1ef33b320
Ahvan Misra <misra28@purdue.edu>
2024-05-08 15:49:24 -0700

    added admin site

--- movielist/admin.py
+++ movielist/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'username']
+    search_fields = ['user']
+
+@admin.register(models.ListEntry)
+class ListEntryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie_id', 'rating', 'date_watched', 'comments']
+    autocomplete_fields = ['movie_id', 'user']
--- mymovielist/settings.py
+++ mymovielist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&9qk^yz4#$6oi&xgph5*b1=t74gcvp(@eof_^5+wfn82iu84a5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '34.218.244.156']
 
