9e451447bdb4fbbf0072f49078e3383a614edcfb
Ahvan Misra <misra28@purdue.edu>
2024-05-08 12:31:43 -0700

    uploaded project

---
+++ MyMovieList-Django+JSON/movielist/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.display_home, name = 'display_home'),
+    path('add_movie/<id>/<poster_extension>/<title>', views.add_movie, name = 'add_movie'),
+    path('query_movie', views.query_movie, name = 'query_movie'),
+    path('list_summary/', views.list_summary, name="list_summary"),
+    path('filter_movies/', views.filter_movies, name="filter_movies"),
+    path('view_movie_info/<movie_id>', views.view_movie_info, name="view_movie_info"),
+    path('update_movie/<movie_id>', views.update_movie, name="update_movie"),
+    path('delete_movie/<movie_id>', views.delete_movie, name="delete_movie"),
+]
+
+"""path('filter_summary/<filtered>/<filtered2>/', views.filter_summary, name="filter-summary"),
+path('filter_summary/<filtered>/', views.filter_summary, name="filter-summary"),
+path('filter_summary/None/<filtered2>/', views.filter_summary, name="filter-summary-none"),"""
