a31fadcad92fbe8db81d68f38c8d59dcefb9fb75
Ahvan Misra <misra28@purdue.edu>
2024-08-08 15:29:21 -0700

    added vercel as allowed host

--- mymovielist/settings.py
+++ mymovielist/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&9qk^yz4#$6oi&xgph5*b1=t74gcvp(@eof_^5+wfn82iu84a5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '34.218.244.156']
+ALLOWED_HOSTS = ['localhost', '34.218.244.156', '.vercel.app']
 
 
 # Application definition
